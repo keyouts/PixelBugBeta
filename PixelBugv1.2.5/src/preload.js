2b0cff23402e117821c6a7e97455a6609c162c3b
keyouts <admin@keyouts.com>
2026-05-18 22:42:03 -0400

    Delete PixelBugv1.2.5 directory

--- PixelBugv1.2.5/src/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("pixelBug", {
-  saveFile: options => ipcRenderer.invoke("save-file", options),
-  openProject: () => ipcRenderer.invoke("open-project")
-});
